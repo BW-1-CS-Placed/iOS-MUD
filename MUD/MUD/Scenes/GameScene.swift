3f0d10b8c72968236d07513ef007917d04823f49
Jordan Christensen <jordan.c4922@gmail.com>
2020-03-03 11:52:48 -0700

    Add xcode project, set up basic game info

---
+++ MUD/MUD/Scenes/GameScene.swift
@@ -0,0 +1,56 @@
+//
+//  GameScene.swift
+//  MUD
+//
+//  Created by Jordan Christensen on 3/3/20.
+//  Copyright © 2020 Mazjap Co. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    var mapNode: SKTileMapNode!
+    
+    override func didMove(to view: SKView) {
+        layoutScene()
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+    
+    func layoutScene() {
+        
+    }
+}
---
+++ MUD/MUD/View Controllers/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  MUD
+//
+//  Created by Jordan Christensen on 3/3/20.
+//  Copyright © 2020 Mazjap Co. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
